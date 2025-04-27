3ecc43723e2746f47dca48baefdf5d14e64340d8
Har1se <timuratai734@gmail.com>
2025-04-27 16:53:53 +0500

    binge, main

--- BingeIterator.java
+++ BingeIterator.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Iterator;
+import java.util.List;
 
-public class BingeIterator {
+public class BingeIterator implements EpisodeIterator {
+    private final List<Season> seasons;
+    private int seasonIndex = 0;
+    private EpisodeIterator currentIterator;
+
+    public BingeIterator(Series series) {
+        this.seasons = series.getSeasons();
+        if (!seasons.isEmpty()) {
+            currentIterator = new SeasonIterator(seasons.get(0));
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (currentIterator != null) {
+            if (currentIterator.hasNext()) {
+                return true;
+            } else if (++seasonIndex < seasons.size()) {
+                currentIterator = new SeasonIterator(seasons.get(seasonIndex));
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Episode next() {
+        return currentIterator.next();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        // Создаем сериал
+        Series series = new Series();
+
+        Season season1 = new Season();
+        season1.addEpisode(new Episode("S1E1", 1800));
+        season1.addEpisode(new Episode("S1E2", 1900));
+        season1.addEpisode(new Episode("S1E3", 2000));
+
+        Season season2 = new Season();
+        season2.addEpisode(new Episode("S2E1", 2100));
+        season2.addEpisode(new Episode("S2E2", 2200));
+
+        series.addSeason(season1);
+        series.addSeason(season2);
+
+        System.out.println("--- Нормальный порядок (1 сезон) ---");
+        for (Episode e : season1) {
+            System.out.println(e);
+        }
+
+        System.out.println("\n--- Обратный порядок (1 сезон) ---");
+        EpisodeIterator reverseIterator = new ReverseSeasonIterator(season1);
+        while (reverseIterator.hasNext()) {
+            System.out.println(reverseIterator.next());
+        }
+
+        System.out.println("\n--- Перемешанный порядок (1 сезон) ---");
+        EpisodeIterator shuffleIterator = new ShuffleSeasonIterator(season1, 42L);
+        while (shuffleIterator.hasNext()) {
+            System.out.println(shuffleIterator.next());
+        }
+
+        System.out.println("\n--- Полный сериал (Binge Mode) ---");
+        EpisodeIterator bingeIterator = new BingeIterator(series);
+        while (bingeIterator.hasNext()) {
+            System.out.println(bingeIterator.next());
+        }
+    }
 }
+
