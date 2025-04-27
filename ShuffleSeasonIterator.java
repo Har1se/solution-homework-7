b08870cac392ef5f6971870bbe4bbe8b1b7a4892
Har1se <timuratai734@gmail.com>
2025-04-27 16:48:11 +0500

    reverse, shuffle, series

---
+++ ShuffleSeasonIterator.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ShuffleSeasonIterator implements EpisodeIterator {
+    private final List<Episode> shuffledEpisodes;
+    private int currentIndex = 0;
+
+    public ShuffleSeasonIterator(Season season, long seed) {
+        shuffledEpisodes = new ArrayList<>(season.getEpisodes());
+        Collections.shuffle(shuffledEpisodes, new Random(seed));
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < shuffledEpisodes.size();
+    }
+
+    @Override
+    public Episode next() {
+        return shuffledEpisodes.get(currentIndex++);
+    }
+}
+
